976fc0820dc68c62dcad03e07ad6e138c34389a7
Jhonny <jhonny9550@gmail.com>
2023-05-16 21:59:52 +1000

    feat: responsive styles added to hero sections
    TODO: make navbar responsive
    TODO: create parent container utility class

--- app/(sections)/Hero.tsx
+++ app/(sections)/Hero.tsx
@@ -2,16 +2,15 @@ import Button from '../components/Button';
 
 export default function Hero() {
   return (
-    <section className="hero-container py-12 px-[124px] gap-6">
+    <section className="hero-container">
       <pre className="text-accent">Hello world! 👋 My name is</pre>
       <h1 className="text-white hero-text">
         Jhonny Martinez
         <span className="block text-headings">I create software products.</span>
       </h1>
-      <p>
-        I’m a <span className="text-primary">Software Engineer</span> with the mission of create{' '}
-        <br />
-        unique products and digital experiences. I’m currently <br /> on the chase of my next role.
+      <p className="sm:max-w-[320px] xl:max-w-[500px]">
+        I’m a <span className="text-primary">Software Engineer</span> with the mission of create
+        unique products and digital experiences. I’m currently on the chase of my next role.
       </p>
       <Button className="mt-[46px]" href="/resume.pdf" target="_blank" download>
         Download my resume
