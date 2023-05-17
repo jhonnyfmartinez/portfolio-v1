6ed713b63870601fe61584b7e1d84b4c0d5fd58d
Jhonny <jhonny9550@gmail.com>
2023-05-17 19:32:48 +1000

    feat: button css file removed
    Using inline tailwind classes instead

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,22 +2,36 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { HTMLAttributeAnchorTarget, PropsWithChildren } from 'react';
 
+type ButtonColor = 'primary';
+
 type ButtonProps = {
   className?: string;
   href: string;
   target?: HTMLAttributeAnchorTarget;
   download?: boolean;
+  color?: ButtonColor;
 };
 
-const Button = (props: PropsWithChildren<ButtonProps>) => (
-  <Link
-    className={clsx(['btn btn-primary', props.className])}
-    href={props.href}
-    target={props.target}
-    download={props.download}
-    rel="noopener roreferrer">
-    {props.children}
-  </Link>
-);
+const buttonColors = {
+  primary: 'border-[2px] border-solid border-primary rounded-lg hover:text-primary',
+};
+
+const Button = ({ color = 'primary', ...props }: PropsWithChildren<ButtonProps>) => {
+  const colorClassName = buttonColors[color];
+  return (
+    <Link
+      className={clsx([
+        'w-fit flex items-center gap-2.5 px-8 py-[18px]',
+        colorClassName,
+        props.className,
+      ])}
+      href={props.href}
+      target={props.target}
+      download={props.download}
+      rel="noopener roreferrer">
+      {props.children}
+    </Link>
+  );
+};
 
 export default Button;
