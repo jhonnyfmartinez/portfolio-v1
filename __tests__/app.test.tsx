dc4a3ebb648697258a46e6f2926b7ec931e2bd5f
Jhonny Martinez <jhonny9550@gmail.com>
2023-05-25 21:47:20 +1000

    feat: adding sections unit testing

--- __tests__/app.test.tsx
+++ __tests__/app.test.tsx
@@ -1,11 +1,15 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '@/app/page';
 import '@testing-library/jest-dom';
 
 describe('App', () => {
   it('App should render', () => {
     render(<App />);
-    // Mocking test until more components are created
-    expect(true).toBe(true);
+
+    const heroSection = screen.getByTestId('hero-section');
+    const aboutSection = screen.getByTestId('about-section');
+
+    expect(heroSection).toBeInTheDocument();
+    expect(aboutSection).toBeInTheDocument();
   });
 });
