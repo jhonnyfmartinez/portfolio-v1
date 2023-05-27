e001bb8203e8c929a7ee1b0b94dca62b6374fcc6
Jhonny Martinez <jhonny9550@gmail.com>
2023-05-27 15:05:18 +1000

    feat: featured section unit test added

--- app/sections/Featured.tsx
+++ app/sections/Featured.tsx
@@ -5,7 +5,7 @@ import { FEATURED_PROJECTS_DATA } from '../utils/data.utils';
 import FeaturedProject from '../components/FeaturedProject';
 
 const Featured = () => (
-  <section className="section-container">
+  <section className="section-container" data-testid="featured-section">
     <SectionHeading icon={TbFolder}>Featured projects</SectionHeading>
     <div className="flex flex-col sm:gap-[124px]">
       {FEATURED_PROJECTS_DATA.map((project, index) => (
