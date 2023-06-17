b4212b57e9e31c1a597d6dfd1a3a994fe4262724
Jhonny Martinez <jhonny9550@gmail.com>
2023-06-17 17:48:52 +1000

    ci: adding unit test for navlinks

--- __tests__/components/NavLinks.test.tsx
+++ __tests__/components/NavLinks.test.tsx
@@ -1,5 +1,5 @@
 import NavLinks from '@/app/components/NavLinks';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 describe('NavLinks', () => {
   it('should render 4 nav links', () => {
@@ -11,5 +11,18 @@ describe('NavLinks', () => {
     expect(linksContainer.children[1].children[0].getAttribute('href')).toBe('/#experience');
     expect(linksContainer.children[2].children[0].getAttribute('href')).toBe('/#projects');
     expect(linksContainer.children[3].children[0].getAttribute('href')).toBe('/#contact');
+  });
+
+  it('should dispatch a close drawer event when a link is clicked', () => {
+    render(<NavLinks />);
+    const linksContainer = screen.getByRole('list');
+    const aboutLink = linksContainer.children[0].children[0];
+
+    const closeDrawerEvent = new CustomEvent('closedrawer');
+    window.dispatchEvent = jest.fn();
+
+    fireEvent.click(aboutLink);
+
+    expect(window.dispatchEvent).toHaveBeenCalledWith(closeDrawerEvent);
   });
 });
